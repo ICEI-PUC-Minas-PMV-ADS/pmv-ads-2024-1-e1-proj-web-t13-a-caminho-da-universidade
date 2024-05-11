05ddce251bbd5294941f9906e73e958cc4820e21
gabriel <gabrielgomeslry@gmail.com>
2024-05-11 18:40:16 -0300

    adicionando universidades com javascript

---
+++ codigo-fonte/assets/js/cursos.js
@@ -0,0 +1,46 @@
+function createUniversity(university) {
+    const courses = document.querySelector('#curso')
+
+    const card = document.createElement('div')
+    card.classList.add('card')
+    card.id = `university-${university.id}`
+    console.log(university.id);
+   
+    const name = document.createElement('h3')
+    name.classList.add('university-name')
+    name.textContent = university.name
+    console.log(university.name);
+
+    const state = document.createElement('h4')
+    state.classList.add('university-state')
+    state.textContent = university.state
+
+    const isPublic = document.createElement('p')
+    isPublic.classList.add('university-public')
+    if (university.isPublic) {
+        isPublic.textContent = "Pública"
+    } else {
+        isPublic.textContent = "Privada"
+    }
+
+    const link = document.createElement('a')
+    link.classList.add('university-link')
+    link.textContent = `Página da universidade`
+    link.href = university.link
+    console.log(university.link);
+
+    card.append(name, state, isPublic, link) 
+    courses.append(card)
+}
+
+async function fetchUniversities() {
+    const universities = await fetch('http://localhost:3000/universidades').then(res => res.json())
+    universities.forEach(createUniversity) 
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    fetchUniversities()
+})
+
+
+
