9ce86627a1ba66b6692e13e0468834f9c0edcf9f
José de Carvalho <decarvalhojose5555@gmail.com>
2024-05-30 11:30:23 -0300

    usuario

--- codigo-fonte/assets/js/usuario.js
+++ codigo-fonte/assets/js/usuario.js
@@ -1,26 +1,28 @@
 var formulario = document.getElementById("formulario");
 
 formulario.addEventListener("submit", function(cadastro) {
-    cadastro.preventDefault();
     
     var name = document.getElementById("name").value;
     var email = document.getElementById("email").value;
     var password = document.getElementById("password").value;
     let users = new Array();
-    const lastGeneratedId = 0;
+    let lastGeneratedId = parseInt(localStorage.getItem("lastId")) || 0;
+    let newId = lastGeneratedId + 1;
+    users.id = newId;
+    localStorage.setItem("lastId", newId);
     
-    if(localStorage.hasOwnProperty(users)) {
-        localStorageGetItem("users");
+    if(localStorage.hasOwnProperty("users")) {
+        localStorage.getItem("users");
         users = JSON.parse(localStorage.getItem("users"));
     }
-
+    
     users.push({
-        id: lastGeneratedId + 1,
+        id: newId,
         name: name, 
         email: email, 
         password: password
     });
-
+    
+    
     localStorage.setItem("users", JSON.stringify(users));
 })
-
