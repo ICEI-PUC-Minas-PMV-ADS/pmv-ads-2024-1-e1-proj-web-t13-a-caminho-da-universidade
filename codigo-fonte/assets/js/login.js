28064b56c9af3ff5459ca0f1b1f067362a82f9ab
Paulo <rpaulocesar44@gmail.com>
2024-06-01 11:30:53 -0300

    Formulário login.js 01-06

---
+++ codigo-fonte/assets/js/login.js
@@ -0,0 +1,13 @@
+function logar (){
+
+     var login = document.getElementById('e-mail').value;
+     var senha = document.getElementById('password').value;
+
+     if(e-mail == "e-mail" && password == "123456"){
+        alert('sucesso');
+        window.location.href = 'curso.html'
+     }else{
+        alert('Usuario ou senha incorretos');
+     }
+}
+
