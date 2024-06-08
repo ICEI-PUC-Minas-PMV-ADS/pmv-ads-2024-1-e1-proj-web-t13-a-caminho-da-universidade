6adc0ed2320002b87f52b215f6fb7fbb431854c0
Paulo <rpaulocesar44@gmail.com>
2024-06-08 12:10:21 -0300

    atualização login.js

--- codigo-fonte/assets/js/login.js
+++ codigo-fonte/assets/js/login.js
@@ -1,25 +1,49 @@
 const email = document.getElementById('e-mail').value;
 const password = document.getElementById('password').value;
 const errorMessage = document.getElementById('errorMessage').value
-const login = document.querySelector('login')
+const login = document.getElementById('login')
 let users = []
+let adm = ''
+
+
 
 if(localStorage.hasOwnProperty("users")) {
    users = JSON.parse(localStorage.getItem("users"));
-   console.log({users});
+   console.log({users})
 }
 
+login.addEventListener('submit', (ev) => {
+   ev.preventDefault()
+   
+   if(!logar(email, password)) {
+      console.log("passei");
+      errorMessage.style.display = 'block'
+      errorMessage.textContent = 'Senha ou email invalidos'   
+   } else {
+      const signIn = {
+         name: name.value,
+         email: email.value,
+         password: password.value,
+         dataNasc: dataNasc.value,
+         sexo: sexo.value,
+         estado: estado.value,
+         isAdmin: isAdmin == codeAdm.value
+      }
+    
+      setTimeout(function() {modalWindow.remove(); window.location.href = "universidade.html";}, 1000);
+   }
+})
 
-function logar (){  
+
+function logar (email, password){  
    for (let i = 0; i < users.length; i++) {
       if(users[i].password === password && users[i].email === email) {
-         setTimeout(function() {modalWindow.remove(); window.location.href = "universidade.html";}, 1000);
-         return  
+         adm = users[i].isAdmin
+         return  true
+      } else {
+         return false
       }
-   }
-
-   errorMessage.style.display = 'block'
-   errorMessage.textContent = 'A senha deve ter no mínimo 8 caracteres e conter pelo menos um caracter especial'    
+   } 
 }
 
 
