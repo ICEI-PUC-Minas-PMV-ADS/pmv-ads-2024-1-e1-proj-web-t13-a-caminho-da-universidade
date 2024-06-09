7b37c0e62ea9b1facd265c4966976ef562a145ce
gabriel <gabrielgomeslry@gmail.com>
2024-06-09 00:15:21 -0300

    feat: add login

--- codigo-fonte/assets/js/login.js
+++ codigo-fonte/assets/js/login.js
@@ -1,36 +1,28 @@
-const email = document.getElementById('e-mail').value;
-const password = document.getElementById('password').value;
-const errorMessage = document.getElementById('errorMessage').value
+const email = document.getElementById('e-mail')
+const password = document.getElementById('password')
+const errorMessage = document.getElementById('errorMessage')
 const login = document.getElementById('login')
 let users = []
-let adm = ''
-
+let user = {}
 
 
 if(localStorage.hasOwnProperty("users")) {
    users = JSON.parse(localStorage.getItem("users"));
-   console.log({users})
+   console.log(users);
 }
 
 login.addEventListener('submit', (ev) => {
    ev.preventDefault()
    
-   if(!logar(email, password)) {
-      console.log("passei");
+   if(!logar(email.value, password.value)) {
       errorMessage.style.display = 'block'
       errorMessage.textContent = 'Senha ou email invalidos'   
    } else {
-      const signIn = {
-         name: name.value,
-         email: email.value,
-         password: password.value,
-         dataNasc: dataNasc.value,
-         sexo: sexo.value,
-         estado: estado.value,
-         isAdmin: isAdmin == codeAdm.value
-      }
-    
-      setTimeout(function() {modalWindow.remove(); window.location.href = "universidade.html";}, 1000);
+      errorMessage.style.display = 'none'
+      localStorage.setItem('logged', JSON.stringify(user))
+      setTimeout(function() {
+         window.location.href = "universidade.html"
+      }, 1000);
    }
 })
 
@@ -38,7 +30,15 @@ login.addEventListener('submit', (ev) => {
 function logar (email, password){  
    for (let i = 0; i < users.length; i++) {
       if(users[i].password === password && users[i].email === email) {
-         adm = users[i].isAdmin
+         user = {
+            name: users[i].name,
+            email: users[i].email,
+            password: users[i].password,
+            birthdate: users[i].dataNasc,
+            gender: users[i].sexo,
+            state: users[i].state,
+            isAdmin: users[i].isAdmin
+         }
          return  true
       } else {
          return false
